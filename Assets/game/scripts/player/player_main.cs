8c0a0b8cf95a086e8778c1e8affc0e1e60047b5b
Null <gabriel.accioly.gatto@gmail.com>
2026-05-09 02:17:59 -0300

    first pass of the player controller finished. has working camera and player movement

--- Assets/game/scripts/player/player_main.cs
+++ Assets/game/scripts/player/player_main.cs
@@ -11,14 +11,21 @@ public class player_main : MonoBehaviour
     public Transform player_camera;
 
     // Player Stats
-    public float cam_sensitivity;
+    public float cam_sensitivity = 200;
+    private float cam_rotation_x;
+    private float cam_rotation_y;
     public float player_speed = 2.5f;
     public int player_health = 100;
 
     // player hud setup
-
     //bool is_game_paused = false;
-   
+    
+    void Start()
+    {
+        // Lock the mouse and turn it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void FixedUpdate()
     {
@@ -26,8 +33,27 @@ public class player_main : MonoBehaviour
         Vector3 player_height = new Vector3 (0, 1, 0);
         player_camera.transform.position = player_body_position.transform.position + player_height;
 
+        PlayerMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Debug.Log("game paused"); // all i need for now
+        }
+        //DrawPauseMenu();
+        MouseLook();
+    }
+
+    void PlayerMove()
+    {
         // move the rigidbody based on camera orientation
         Vector3 cam_orientation = player_camera.transform.forward;
+        Vector3 cam_side_orientation = player_camera.transform.right;
 
         if (Input.GetKey("w"))
         {
@@ -37,14 +63,24 @@ public class player_main : MonoBehaviour
         {
             player_rb.AddForce(-cam_orientation * player_speed);
         }
+        if (Input.GetKey("a"))
+        {
+            player_rb.AddForce(-cam_side_orientation * player_speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            player_rb.AddForce(cam_side_orientation * player_speed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void MouseLook()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Debug.Log("game paused"); // all i need for now
-        }
+        float mouse_x = Input.GetAxisRaw("Mouse X") * Time.deltaTime * cam_sensitivity;
+        float mouse_y = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * cam_sensitivity;
+        
+        cam_rotation_y += mouse_x;
+        cam_rotation_x -= mouse_y;
+
+        player_camera.transform.rotation = Quaternion.Euler(cam_rotation_x, cam_rotation_y, 0);
     }
 }
